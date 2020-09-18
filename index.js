573efabc1dbfa7da896a82a5c2c42f42ca7bef13
Denise Kruse <deniselkruse@gmail.com>
2020-09-18 11:58:53 -0400

    second commit

--- index.js
+++ index.js
@@ -2,14 +2,13 @@ const baseURL = 'https://api.spoonacular.com/recipes/complexSearch';
 const key = '2dbc75257fac4190bf31c59b6f2d347c';
 let url;
 
-//SEARCH FORM
-
 const searchTerm = document.querySelector('.search');
 const searchForm = document.querySelector('form');
 const submitBtn = document.querySelector('.submit');
-const section = document.querySelector('section');
 
-//RESULTS SECTION
+const recipe = document.getElementById("recipe");
+const ingredientList = document.getElementById("ingredients");
+
 searchForm.addEventListener('submit', fetchResults);
 
 function fetchResults(e) {
@@ -18,28 +17,67 @@ function fetchResults(e) {
     url = baseURL + '?apiKey=' + key + '&query=' + searchTerm.value + '&addRecipeInformation=true';
 
     fetch(url)
-    .then(res => res.json())    
-    .then(json => display(json))
+        .then(res => res.json())
+        .then(json => displayRecipe(json))
 }
 
-function display(json) {    
+function displayRecipe(json) {
     console.log(json);
+    while (recipe.firstChild) {
+        recipe.removeChild(recipe.firstChild);
+    }
+    while (ingredientList.firstChild) {
+        ingredientList.removeChild(ingredientList.firstChild);
+    }
 
-    let row = document.getElementsByClassName('row no-gutters');
+    let recipeInfo = json.results;
 
-    let card = document.getElementsByClassName('card mb-3');
+    if (recipeInfo.length === 0) {
+        console.log("No Results");
+    } else {
+        for (let i = 0; i < recipeInfo.length; i++) {
+            
+            // Math.floor(Math.random() * recipeInfo.length);
 
-    let img = document.getElementById("card-image");
-    img.src = json.results[1].image;
-    console.log(json.results[1].image);
-    console.log(img);
+            let row = document.getElementsByClassName('row no-gutters');
 
-    let cardBody = document.getElementsByClassName('card-body');
+            let card = document.getElementsByClassName('card mb-3');
 
-    let cardTitle = document.getElementById("cardTitle");
-    cardTitle.innerText = json.results[1].title;
-    console.log(json.results[1].title);
+            let img = document.getElementById("card-image");
+            img.src = json.results[i].image;
+            // console.log(json.results[1].image);
+            // console.log(img);
 
-    let cardText = document.getElementById('cardText');
-    cardText.innerText = json.results[1].analyzedInstructions[0].steps;
+            let cardBody = document.getElementsByClassName('card-body');
+
+            let cardTitle = document.getElementById("cardTitle");
+            cardTitle.innerText = json.results[i].title;
+            // console.log(json.results[1].title);
+
+            directions = json.results[i].analyzedInstructions[0].steps;
+            // console.log(directions);
+
+            directions.forEach(element => {
+                console.log(element.step);
+                let direction = document.createElement('li');
+                direction.innerText = element.step;
+                recipe.appendChild(direction);
+            });
+
+            components = json.results[i].analyzedInstructions[0].steps[i].ingredients;
+            // console.log(components);
+
+            components.forEach(ingr => {
+                console.log(ingr.name);
+                let ingredientName = document.createElement('li');
+                ingredientName.innerText = ingr.name;
+                ingredientList.appendChild(ingredientName);
+            });
+
+            document.getElementById('ingr').innerHTML = 'Ingredients';
+
+            document.getElementById('dir').innerHTML = 'Directions';
+        }
+    }
 }
+
